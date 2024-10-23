f67e01714e4639a9e0e626ab1d43c2555c5572ed
Jumali Projek <jumaliudin7@gmail.com>
2024-10-23 08:05:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const products = [
         name: "Rak Gudang Heavy Duty",
         image: "img1.png",
         description: "Rak kuat untuk penyimpanan barang berat.",
-        price: "Rp 1.500.000",
+        price: "Rp 9.999.999",
     },
     {
         name: "Rak Mini Market",
         image: "img2.png",
         description: "Rak fungsional untuk menampilkan produk.",
-        price: "Rp 750.000",
+        price: "Rp 2.750.000",
     },
     {
         name: "Rak Display Stylish",
         image: "img3.png",
         description: "Rak untuk menonjolkan produk unggulan.",
-        price: "Rp 900.000",
+        price: "Rp 2.500.000",
     },
     {
         name: "SC-303",
         image: "img4.png",
-        description: "Troli yang dapat dilipat sehingga menghemat tempat. Cocok untuk minimarket.",
-        price: "Rp 600.000",
+        description: "Troli yang dapat dilipat sehingga menghemat tempat.",
+        price: "Rp 1.500.000",
     }
 ];
 
